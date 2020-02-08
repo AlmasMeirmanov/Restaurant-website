21f4060182426a172e1615a02d327a0271624762
Almas Meirmanov <almas1521@gmail.com>
2020-02-07 16:08:08 -0800

    section5

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+  var carouselImages = [
+    {
+      image_url: "./Assets/alcohol-alcoholic-bar-beverage-4784.jpg"
+    },
+    {
+      image_url: "./Assets/assorted-wine-bottles-1283219 (2).jpg"
+    },
+    {
+      image_url: "./Assets/bar-billiards-gambling-game-261043.jpg"
+    },
+    {
+      image_url:
+        "./Assets/depth-of-field-photo-of-two-pilsner-glasses-681847.jpg"
+    },
+    {
+      image_url: "./Assets/alcohol-alcoholic-background-beer-301692.jpg"
+    }
+  ]
+  var htmlStr = ""
+  carouselImages.forEach(function(item, i) {
+    htmlStr += `
+       <div id="image${i}" class="pics">
+          <div><img src="${item.image_url}" /></div>
+           <div class= "prev">&laquo</div>
+           <div class= "next">&raquo</div>
+       </div>
+       `
+  })
+  $(".section5").html(htmlStr)
+  $(".pics:first-child").addClass("current opacity")
+
+  $(".next").on("click", function() {
+    var current = $(".current")
+      .attr("id")
+      .substr(5)
+    var nextIndex = Number(current) + 1
+
+    if (nextIndex === carouselImages.length) {
+      nextIndex = 0
+    }
+
+    $(`#image${current}`).removeClass("current opacity")
+    $(`#image${nextIndex}`).addClass("current opacity")
+  })
+  $(".prev").on("click", function() {
+    var current = $(".current")
+      .attr("id")
+      .substr(5)
+    var prevIndex = Number(current) - 1
+
+    if (prevIndex === -1) {
+      prevIndex = carouselImages.length - 1
+    }
+
+    $(`#image${current}`).removeClass("current opacity")
+    $(`#image${prevIndex}`).addClass("current opacity")
+  })
+})
